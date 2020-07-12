2163f209291ecd596eac573bf07be43893beffd8
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-12 16:30:20 +0300

    add npms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import {View, Panel, PanelHeader, Header, Group, Cell} from '@vkontakte/vkui'
+import '@vkontakte/vkui/dist/vkui.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View activePanel="main">
+    <Panel id="main">
+      <PanelHeader>VKUI</PanelHeader>
+      <Group header={<Header mode="secondary">Items</Header>}>
+        <Cell>Hello</Cell>
+        <Cell>World</Cell>
+      </Group>
+    </Panel>
+  </View>
   );
 }
 
