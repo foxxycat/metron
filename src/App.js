c51af2567a170c5d1047cd324b40442432df6b42
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-12 17:34:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import {View, Panel, PanelHeader, Header, Group, Cell, Button, Div, FormLayout, Slider} from '@vkontakte/vkui'
+import {View, Panel, PanelHeader, Header, Group, Cell, Button, Div, FormLayout, Slider, platform, Root} from '@vkontakte/vkui'
 import '@vkontakte/vkui/dist/vkui.css';
 import './App.css';
 
-function App() {
-  return (
-    <View activePanel="main">
-    <Panel id="main">
-      <PanelHeader>Metron</PanelHeader>
-      <Div className='Button-wrapper'>
-        <Button className='Main-button' mode='outline' size='l'>
-          off
-        </Button>
-      </Div>
-      <Group header={<Header mode="secondary">Настройки</Header>}>
-        <FormLayout>
-          <Slider min={0} max={100} />
-        </FormLayout>
-      </Group>
-    </Panel>
-  </View>
-  );
+// function App() {
+//   return (
+//     <View activePanel="main">
+//     <Panel id="main">
+//       <PanelHeader>Metron</PanelHeader>
+//       <Div className='Button-wrapper'>
+//         <Button className='Main-button' mode='outline' size='l'>
+//           off
+//         </Button>
+//       </Div>
+//       <Group header={<Header mode="secondary">Настройки</Header>}>
+//         <FormLayout>
+//           <Slider min={0} max={100} />
+//         </FormLayout>
+//       </Group>
+//     </Panel>
+//   </View>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = App.getInitState();
+  }
+
+  static getInitState() {
+    return {
+      activePanel: 'homePanel'
+    }
+  }
+
+  render() {
+    const osname = platform();
+
+    return (
+      <Root activeView="homeView">
+        <View activePanel="main">
+          <Panel id="homePanel" activePanel={this.state.activePanel}>
+            <PanelHeader>Metron</PanelHeader>
+            <Div className='Button-wrapper'>
+              <Button className='Main-button' mode='outline' size='l'>
+                off
+              </Button>
+            </Div>
+            <Group header={<Header mode="secondary">Настройки</Header>}>
+              <FormLayout>
+                <Slider min={0} max={100} />
+              </FormLayout>
+            </Group>
+          </Panel>
+        </View>
+    </Root>
+    );
+  }
+
 }
 
 export default App;
