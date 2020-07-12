e6888ba31d955da5ea8b9228c4456c66ebc1d102
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-12 15:08:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,32 +8,14 @@ import Home from './panels/Home';
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
 
 	const go = e => {
 		setActivePanel(e.currentTarget.dataset.to);
 	};
 
 	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
+		<View activePanel={activePanel} >
+			<Home id='home' go={go} />
 		</View>
 	);
 }
