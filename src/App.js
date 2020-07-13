88912557911b35619e1778c6012403f63ca41075
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-13 23:21:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,48 +5,80 @@ import '@vkontakte/vkui/dist/vkui.css';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = App.getInitState();
-  }
-
-  static getInitState() {
-    return {
-      bpm: 0,
-      playing: false,
-      count: 0,
-      activePanel: 'homePanel'
-    };
-  }
 
   render() {
-    const osname = platform();
-
-  return (
-    //<Root activeView="homeView">
-      <View activePanel="homeView" activePanel={this.state.activePanel}>
-        <Panel id="homePanel">
+    return(
+      <View activePanel="main">
+        <Panel id="main">
           <PanelHeader>Metron</PanelHeader>
-          <Div className='Button-wrapper'>
-            <Button className='Main-button' mode='outline' onClick={this.props.handleClick}>
-              {this.props.currentState ? "On" : "Off"}
-            </Button>
-          </Div>
-          <Group header={<Header mode="secondary">Настройки</Header>}>
-            <FormLayout>
-              <Slider min={0} max={100} />
-            </FormLayout>
-          </Group>
+          <Button>
+            
+          </Button>
         </Panel>
       </View>
-    //</Root>
     );
   }
 
-  openMain() {
-    this.setState({activePanel:'homePanel'});
-  }
+  // constructor(props) {
+  //   super(props);
+
+  //   this.click1 = new Audio("https://daveceddia.com/freebies/react-metronome/click1.wav");
+  //   this.click2 = new Audio("https://daveceddia.com/freebies/react-metronome/click1.wav");
+    
+  //   this.state = App.getInitState();
+  // }
+
+  // static getInitState() {
+  //   return {
+  //     bpm: 0,
+  //     playing: false,
+  //     count: 0,
+  //     activePanel: 'homePanel',
+
+      
+  //   };
+  // }
+
+  // updateInterval() {
+  //   const bpmSpeed = 60 * 1000 / this.state.bpm;
+  //   this.timer = setInterval(this.playClick, bpmSpeed);
+  // }
+
+  // playClick() {
+  //   if (this.state.count === 0) this.click2.play();
+  //   else this.click1.play();
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   });
+  // }
+
+  // render() {
+  //   const osname = platform();
+
+  // return (
+  //   //<Root activeView="homeView">
+  //     <View activePanel="homeView" activePanel={this.state.activePanel}>
+  //       <Panel id="homePanel">
+  //         <PanelHeader>Metron</PanelHeader>
+  //         <Div className='Button-wrapper'>
+  //           <Button className='Main-button' mode='outline' handleClick={}>
+  //             {this.props.currentState ? "On" : "Off"}
+  //           </Button>
+  //         </Div>
+  //         <Group header={<Header mode="secondary">Настройки</Header>}>
+  //           <FormLayout>
+  //             <Slider min={0} max={100} />
+  //           </FormLayout>
+  //         </Group>
+  //       </Panel>
+  //     </View>
+  //   //</Root>
+  //   );
+  // }
+
+  // openMain() {
+  //   this.setState({activePanel:'homePanel'});
+  // }
 
 }
 
