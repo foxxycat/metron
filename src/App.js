8bb36d52bda60720928ed22829b6ae0b0ae636c7
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-13 22:47:14 +0300

    Summary

--- src/App.js
+++ src/App.js
@@ -4,66 +4,51 @@ import {View, Panel, PanelHeader, Header, Group, Cell, Button, Div, FormLayout, 
 import '@vkontakte/vkui/dist/vkui.css';
 import './App.css';
 
-// function App() {
-//   return (
-//     <View activePanel="main">
-//     <Panel id="main">
-//       <PanelHeader>Metron</PanelHeader>
-//       <Div className='Button-wrapper'>
-//         <Button className='Main-button' mode='outline' size='l'>
-//           off
-//         </Button>
-//       </Div>
-//       <Group header={<Header mode="secondary">Настройки</Header>}>
-//         <FormLayout>
-//           <Slider min={0} max={100} />
-//         </FormLayout>
-//       </Group>
-//     </Panel>
-//   </View>
-//   );
-// }
-
 class App extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = App.getInitState();
-  }
+  //   this.state = App.getInitState();
+  // }
 
-  static getInitState() {
-    return {
-      activePanel: 'homePanel'
-    };
-  }
+  // static getInitState() {
+  //   return {
+  //     bpm: 0,
+  //     playing: false,
+  //     count: 0,
+  //     activePanel: 'homePanel'
+  //   };
+  // }
+
+
 
   render() {
     const osname = platform();
 
-    return (
-      //<Root activeView="homeView">
-        <View activePanel="homeView" activePanel={this.state.activePanel}>
-          <Panel id="homePanel">
-            <PanelHeader>Metron</PanelHeader>
-            <Div className='Button-wrapper'>
-              <Button className='Main-button' mode='outline' size='l'>
-                off
-              </Button>
-            </Div>
-            <Group header={<Header mode="secondary">Настройки</Header>}>
-              <FormLayout>
-                <Slider min={0} max={100} />
-              </FormLayout>
-            </Group>
-          </Panel>
-        </View>
+  return (
+    //<Root activeView="homeView">
+      <View activePanel="homeView" activePanel={this.state.activePanel}>
+        <Panel id="homePanel">
+          <PanelHeader>Metron</PanelHeader>
+          <Div className='Button-wrapper'>
+            <Button className='Main-button' mode='outline'>
+              
+            </Button>
+          </Div>
+          <Group header={<Header mode="secondary">Настройки</Header>}>
+            <FormLayout>
+              <Slider min={0} max={100} />
+            </FormLayout>
+          </Group>
+        </Panel>
+      </View>
     //</Root>
     );
   }
 
-  openMain() {
-    this.setState({activePanel:'homePanel'});
-  }
+  // openMain() {
+  //   this.setState({activePanel:'homePanel'});
+  // }
 
 }
 
