b0ff9d14ae516ce2319eb93a2d9c539069666c7b
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-18 17:37:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,269 +1,148 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import {View, Panel, PanelHeader, Header, Group, Div, FormLayout, Button, Slider, Text} from '@vkontakte/vkui'
 import '@vkontakte/vkui/dist/vkui.css';
 import './App.css';
 import click1 from '../src/samples/click1.wav'
 import click2 from '../src/samples/click2.wav'
+import { render } from '@testing-library/react';
 
-// class App extends Component {
+//class App extends React.Component{
 
-//   constructor(props) {
-//     super(props);
-    
-//     this.click1 = new Audio(click1);
-//     this.click2 = new Audio(click2);
-//     // this.handleBPM = this.handleBPM.bind(this);
-//     // this.updateInterval = this.updateInterval.bind(this);
-//     // this.startStop = this.startStop.bind(this);
-//     // this.playClick = this.playClick.bind(this);
-//     this.state = App.getInitState();
-//   }
+  // V 0.01
+  //
+  // constructor(props) {
+  //   super(props);
 
-//   static getInitState() {
-//     return {
-//       bpm: 100,
-//       playing: false,
-//       count: 0,
-//       activePanel: 'homePanel',
-//       beatsPerMeasure: 4
-//     };
-//   }
+  //   this.state = {
+  //     // название на кнопке
+  //     label: 'on',
+  //     // бпм
+  //     tempoBpm: 100,
 
-//   // updateInterval() {
-//   //   const bmpSpeed = 60 * 1000 / this.state.bpm;
-//   //   this.timer = setInterval(this.playClick, bmpSpeed);
-//   // }
+  //     timer: null,
+  //     counter: 0
+  //   };
 
-//   // handleBPM(event) {
-//   //   const bpm = event.target.value;
-//   //   if (this.state.playing) {
-//   //       clearInterval(this.timer);
-//   //       this.updateInterval();
-//   //       this.setState({
-//   //           count: 0,
-//   //           bpm
-//   //       });
-//   //   } else {
-//   //       this.setState({
-//   //           bpm
-//   //       });
-//   //   };
-//   // }
+  //   // добавление аудио
+  //   this.click1 = new Audio(click1);
 
-//   handleBpmChange = event =>{
-//     const bpm = event.target.value;
-//     if(this.state.playing){
-//       clearInterval(this.timer);
-//       this.timer = setInterval(this.playClick, (60/ bpm) *1000);
-//       this.setState({
-//         count:0,
-//         bpm
-//       });
-//     }else{
-//       this.setState({bpm});
-//     }
-//   }
+  //   // обработчик кнопки
+  //   this.toggleMainBtn = this.toggleMainBtn.bind(this);
+  //   this.startTimer = this.startTimer.bind(this);
+  //   this.tick = this.tick.bind(this)
+  //   this.stopTimer = this.stopTimer.bind(this)
+  // }
 
-// // playClick() {
-// //     if (this.state.count === 0) this.click2.play();
-// //     else this.click1.play();
-// //     this.setState({
-// //         count: this.state.count + 1
-// //     });
-// //   }
+  // startTimer() {
+  //   let timer = setInterval(this.tick, (60/this.state.tempoBpm*1000));
+  //   this.setState({timer});
+  // }
 
-// playClick = () =>{
-//   const{count, beatsPerMeasure} = this.state;
+  // stopTimer() {
+  //   clearInterval(this.state.timer);
+  // }
 
-//   if(count% beatsPerMeasure === 0){
-//     this.click2.play();
-//   }else{
-//     this.click1.play();
-//   }
-//   this.setState(state => ({
-//     count: (state.count + 1) % state.beatsPerMeasure
-//   }));
-// };
+  // tick() {
+  //   this.click1.play()
+  // }
 
-//   // startStop() {
-//   //   if (this.state.playing) {
-//   //       clearInterval(this.timer);
-//   //       this.setState({
-//   //           playing: false
-//   //       });
-//   //   } else {
-//   //       this.updateInterval();
-//   //       this.setState({
-//   //           count: 0,
-//   //           playing: true
-//   //       }, this.playClick)
-//   //   }
-//   // }
+  // // метод обработки нажатия на кнопку
+  // toggleMainBtn() {
+  //   let label = this.state.label === 'off' ? /*tempoBpm*/ 'on' : 'off';
 
-//   startStop = () => {
-//     if(this.state.playing){
-//       clearInterval(this.timer);
-//       this.setState({
-//         playing:false
-//       });
-//     }else{
-//       this.timer = setInterval(
-//         this.playClick,
-//         (60/this.state.bpm)*1000
-//       );
-//       this.setState({
-//         count:0,
-//         playing:true,
-//       },
-//       this.playClick
-//       );
-//     }
-//   };
+  //   if(this.state.label === 'on') {
+  //     this.click1.play()
 
-//   render() {
-//     const osname = platform();
-//     const {playing, bpm} = this.state
+  //     this.startTimer()
+  //   }
+  //   else {
+  //     this.stopTimer()
+  //   }
+  //   this.setState({label: label});
+  // }
 
-//   return (
-//     //<Root activeView="homeView">
-//       <View activePanel="homeView" activePanel={this.state.activePanel}>
-//         <Panel id="homePanel">
-//           <PanelHeader>Metron</PanelHeader>
-//           <Div className='Main-wrapper'>
-//             {/* <Button handleClick={this.startStop} currentState={this.state.playing}/> */}
-//             <Button mode='outline' className='Main-button' onClick={this.startStop}>
-//               {
-//                 this.playing ? 'Stop' : 'Start'
-//               }
-//             </Button>
-//           </Div>
-
-//           <Group header={<Header mode="secondary">Настройки</Header>}>
-//             <FormLayout>
-//               {/* <Slider bpm={this.state.bpm} handleChange={this.handleBPM}/> */}
-//               <Slider min={60} max={240} value={bpm} onChange={this.handleBpmChange}></Slider>
-//             </FormLayout>
-//           </Group>
-//         </Panel>
-//       </View>
-//     //</Root>
-//     );
-//   }
-
-//   openMain() {
-//     this.setState({activePanel:'homePanel'});
-//   }
-
-// }
-
-// class Button extends React.Component {
-//   render() {
-//     return(
-//       <button onClick={this.props.handleClick} className="Main-button">
-//         {
-//           this.props.currentState ? "Stop" : "Start"
-//         }
-//       </button>
+  // рендеринг страницы
+  // render() {
+  //   return(
+      // V 0.01
+      //
+      // <View activePanel='Main'>
+      //   <Panel id='Main'>
+      //     <PanelHeader>Metron</PanelHeader>
+      //     <Div className='Main-wrapper'>
+      //       <Button mode='outline' className='Main-button' onClick={this.toggleMainBtn}>
+      //         {this.state.label}
+      //       </Button>
+      //     </Div>
+      //     <Group header={<Header mode='secondary'>Настройки</Header>}>
+      //         <FormLayout>
+      //           <Text weight='regular' style={{textAlign:'center'}}>
+      //             {this.state.tempoBpm} bpm
+      //           </Text>
+      //           <Slider min={60} max={240} step={1} /*top={this.state.tempoBpm }*/ value={Number(this.state.tempoBpm)} onChange={tempoBpm => this.setState({tempoBpm})}></Slider>
+      //         </FormLayout>
+      //       </Group>
+      //   </Panel>
+      // </View>
 //     );
 //   }
 // }
 
-// class Slider extends React.Component {
-//   render() {
-//     return(
-//       <div id="bpm-slider">
-//         <div>
-//           {
-//             this.props.bpm
-//           }
-//           BPM
-//         </div>
-//         <input type="range" min="60" max="240" value={this.props.bpm} onChange={this.props.handleChange}></input>
-//       </div>
-//     );
-//   }
-// }
-
-class App extends React.Component{
-
+class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // название на кнопке
-      label: 'on',
-      // бпм
-      tempoBpm: 100,
+      isToggleOn: false,
+      tempo: 100
+    }
 
-      timer: null,
-      counter: 0
-    };
-
-    // добавление аудио
     this.click1 = new Audio(click1);
-
-    // обработчик кнопки
-    this.toggleMainBtn = this.toggleMainBtn.bind(this);
-    this.startTimer = this.startTimer.bind(this);
-    this.tick = this.tick.bind(this)
-    this.stopTimer = this.stopTimer.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  startTimer() {
-    let timer = setInterval(this.tick, (60/this.state.tempoBpm*1000));
-    this.setState({timer});
+  timerStart() {
+    this.timerID = setInterval(() => this.tick(),(60/this.state.tempo*1000))
   }
 
-  stopTimer() {
-    clearInterval(this.state.timer);
+  timerStop() {
+    clearInterval(this.timerID)
   }
 
   tick() {
-    this.click1.play()
+    this.click1.play();
   }
 
-  // метод обработки нажатия на кнопку
-  toggleMainBtn() {
-    let label = this.state.label === 'off' ? /*tempoBpm*/ 'on' : 'off';
-
-    if(this.state.label === 'on') {
-      this.click1.play()
-
-      this.startTimer()
-    }
-    else {
-      this.stopTimer()
-    }
-    this.setState({label: label});
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }), (this.state.isToggleOn ? this.timerStop : this.timerStart));
   }
 
-  changeBpm() {
-
-  }
-
-  // рендеринг страницы
   render() {
     return(
       <View activePanel='Main'>
         <Panel id='Main'>
           <PanelHeader>Metron</PanelHeader>
           <Div className='Main-wrapper'>
-            <Button mode='outline' className='Main-button' onClick={this.toggleMainBtn}>
-              {this.state.label}
+            <Button mode='outline' className='Main-button' onClick={()=>this.handleClick()}>
+              {this.state.isToggleOn ? 'off': 'on'}
             </Button>
           </Div>
           <Group header={<Header mode='secondary'>Настройки</Header>}>
               <FormLayout>
                 <Text weight='regular' style={{textAlign:'center'}}>
-                  {this.state.tempoBpm} bpm
+                  {this.state.tempo} bpm
                 </Text>
-                <Slider min={60} max={240} step={1} /*top={this.state.tempoBpm }*/ value={Number(this.state.tempoBpm)} onChange={tempoBpm => this.setState({tempoBpm})}></Slider>
+                <Slider min={60} max={240} step={1} /*top={this.state.tempoBpm }*/ value={Number(this.state.tempo)} onChange={tempo => this.setState({tempo})}></Slider>
               </FormLayout>
             </Group>
         </Panel>
       </View>
     );
-  }
+  }  
 }
+
+
 
 export default App;
